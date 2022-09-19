06f50c07a2eca1866fd1eaa6dcb05225e612409c
CC3301 <florianfink85@gmail.com>
2022-09-20 00:13:07 +0200

    add registry.fink.home ssl certificate, update wireguard status dashboard

--- scripts/generate_csr.sh
+++ scripts/generate_csr.sh
@@ -2,6 +2,6 @@
 #openssl req -new -newkey rsa:4096 -nodes -keyout temp/grafana.fink.home.key    -out temp/grafana.fink.home.csr     -subj "/C=DE/ST=Bavaria/O=KFnet/CN=grafana.fink.home"
 #openssl req -new -newkey rsa:4096 -nodes -keyout temp/prometheus.fink.home.key -out temp/prometheus.fink.home.csr  -subj "/C=DE/ST=Bavaria/O=KFnet/CN=prometheus.fink.home"
 #openssl req -new -newkey rsa:4096 -nodes -keyout temp/influxdb.fink.home.key   -out temp/influxdb.fink.home.csr    -subj "/C=DE/ST=Bavaria/O=KFnet/CN=influxdb.fink.home"
-#openssl req -new -newkey rsa:4096 -nodes -keyout temp/cadvisor.fink.home.key - out temp/cadvisor.fink.home.csr     -subj "/C=DE/ST=Bavaria/O=KFnet/CN=cadvisor.fink.home"
+#openssl req -new -newkey rsa:4096 -nodes -keyout temp/cadvisor.fink.home.key   -out temp/cadvisor.fink.home.csr    -subj "/C=DE/ST=Bavaria/O=KFnet/CN=cadvisor.fink.home"
 #openssl req -new -newkey rsa:4096 -nodes -keyout temp/loki.fink.home.key       -out temp/loki.fink.home.csr        -subj "/C=DE/ST=Bavaria/O=KFnet/CN=loki.fink.home"
-
+#openssl req -new -newkey rsa:4096 -nodes -keyout temp/registry.fink.home.key   -out temp/registry.fink.home.csr    -subj "/C=DE/ST=Bavaria/O=KFnet/CN=registry.fink.home"
